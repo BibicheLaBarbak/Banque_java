37696bbe6495ce6aa2ceb4aac0756a39364f4821
Barbak <chassaing.hugo@outlook.fr>
2020-08-31 23:58:42 +0200

    Amélioration du code + ajout if else pour option 1 et 2

--- banque_start.java
+++ banque_start.java
@@ -4,9 +4,10 @@ public class banque_start {
     public static void main(String[] arg) {
 
         // Déclarations des données
-        String TypeCpt, Theme, MoyenPaie, NumCpt;
+        String TypeCpt = "Courant", Theme = "Loyer", MoyenPaie = "CB";
+        long NumCpt = 250230;
         int Choix;
-        float ValeurOp, Taux;
+        float ValeurOp = 1400, Taux = 10;
         Date Date;
 
         // Affichage menu + choix de l'option
@@ -20,34 +21,57 @@ public class banque_start {
         Choix = LectureClavier.nextInt();
 
         // Affichage du menu des options
-        if (Choix == 1) {
-            System.out.println("Option 1");
-            System.out.println("Type du compte [Types possibles : courant, joint, épargne] : ");
-            TypeCpt = LectureClavier.next();
-            System.out.println("Numero du compte : ");
-            NumCpt = LectureClavier.next();
-            System.out.println("Première valeur créditée : ");
-            ValeurOp = LectureClavier.nextFloat();
-            System.out.println("Taux de placement : ");
-            Taux = LectureClavier.nextFloat();
+        switch (Choix) {
+            case 1:
+                System.out.println("Type du compte [Types possibles : courant, joint, épargne] : ");
+                    TypeCpt = LectureClavier.next();
+                if (TypeCpt == "Courant" || TypeCpt == "Joint") {
+                    System.out.println("Numero du compte : ");
+                    NumCpt = LectureClavier.nextLong();
+                    System.out.println("Première valeur créditée : ");
+                    ValeurOp = LectureClavier.nextFloat();
+                } else {
+                    System.out.println("Numero du compte : ");
+                    NumCpt = LectureClavier.nextLong();
+                    System.out.println("Première valeur créditée : ");
+                    ValeurOp = LectureClavier.nextFloat();
+                    System.out.println("Taux de placement : ");
+                    Taux = LectureClavier.nextFloat();
+                }
+                break;
 
-        } else if (Choix == 2) {
-            System.out.println("Compte à consulter : ");
-            NumCpt = LectureClavier.next();
+            case 2:
+                System.out.println("Compte à consulter : ");
+                NumCpt = LectureClavier.nextLong();
+                if (NumCpt == 250230) {
+                    System.out.println("Numéro de compte ouvert : \"250230\" ");
+                    System.out.println("Type de compte : " +TypeCpt);
+                    System.out.println("Valeur initiale : " +ValeurOp);
+                } else {
+                    System.out.println("Ce compte n'existe pas !");
+                }
+                break;
 
-        } else if (Choix == 3) {
-            System.out.println("Option 3 : Option non programmée ");
+            case 3:
+                System.out.println("Option 3 : Option non programmée ");
+                break;
 
-        } else if (Choix == 4) {
-            System.exit(0);
+            case 4:
+                System.exit(0);
+                break;
 
-        } else if (Choix == 5) {
-            System.out.println("L'option 1 vous permet de creer un compte en choisissant son type, son numero et sa premiere valeur");
-            System.out.println("L'option 2 vous permet de consulter un compte déjà existant");
-            System.out.println("L'option 3 n'est pas encore programmée");
-            System.out.println("L'option 4 vous permet de fermer l'application");
+            case 5:
+                System.out.println(
+                        "L'option 1 vous permet de creer un compte en choisissant son type, son numero et sa premiere valeur");
+                System.out.println("L'option 2 vous permet de consulter un compte déjà existant");
+                System.out.println("L'option 3 n'est pas encore programmée");
+                System.out.println("L'option 4 vous permet de fermer l'application");
+                break;
+
+            default:
+                System.out.println("Vous n'avez sélectionné aucune des 5 options !");
+                break;
         }
-
         LectureClavier.close();
     }
 }
